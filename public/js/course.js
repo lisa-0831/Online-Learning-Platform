e16dfda1c4a85a237cdfb901c88a83be0807c2b7
PEI-WEN WANG <peiwen0831@gmail.com>
2022-07-04 22:24:49 +0800

    feat: add rating feature

--- public/js/course.js
+++ public/js/course.js
@@ -12,7 +12,15 @@ window.onload = async function () {
   const courseObj = await courseRes.json();
 
   const detailsObj = courseObj.details;
-  const commentsArr = courseObj.comments;
+  console.log(detailsObj);
+  const questionsArr = courseObj.questions;
+  console.log(questionsArr);
+  const ratingArr = courseObj.rating;
+  console.log(19, ratingArr);
+  const discussionArr = courseObj.discussion;
+  console.log(discussionArr);
+  const status = courseObj.status;
+  console.log(status);
 
   // Course Video
   let courseVideo = document.createElement("source");
@@ -22,11 +30,20 @@ window.onload = async function () {
 
   // Course Information
 
+  // Course Price
+  let priceDiv = document.createElement("div");
+  priceDiv.setAttribute("class", "course-info course-price");
+  priceDiv.innerHTML = `<p>課程售價</p>
+  <h2>NT$${detailsObj["price"]}</h2>`;
+  document.getElementsByClassName("course-all-info")[0].prepend(priceDiv);
+
   // Course Title
-  let courseTitle = document.createElement("h2");
-  courseTitle.setAttribute("class", "course-title");
+  let titleDiv = document.createElement("div");
+  titleDiv.setAttribute("class", "course-info course-title");
+  let courseTitle = document.createElement("h1");
   courseTitle.innerText = detailsObj["title"];
-  document.getElementById("course-info").appendChild(courseTitle);
+  titleDiv.appendChild(courseTitle);
+  document.getElementsByClassName("course-all-info")[0].prepend(titleDiv);
 
   // Course Upload Time
   let date = new Date(detailsObj["upload_time"]);
@@ -37,91 +54,182 @@ window.onload = async function () {
   let courseUploadTime = document.createElement("p");
   courseUploadTime.setAttribute("class", "course-upload-time");
   courseUploadTime.innerText = uploadDate;
-  document.getElementById("course-info").appendChild(courseUploadTime);
+  document.getElementById("detail").appendChild(courseUploadTime);
 
   // Course Teacher
   let courseTeacher = document.createElement("p");
   courseTeacher.setAttribute("class", "course-teacher");
   courseTeacher.innerText = `開課老師：${detailsObj["name"]}`;
-  document.getElementById("course-info").appendChild(courseTeacher);
+  document.getElementById("detail").appendChild(courseTeacher);
 
   // Course Description
   let courseDescription = document.createElement("p");
   courseDescription.setAttribute("class", "course-description");
   courseDescription.innerText = detailsObj["description"];
-  document.getElementById("course-info").appendChild(courseDescription);
+  document.getElementById("detail").appendChild(courseDescription);
 
-  // Comments
+  // Questions
   document.getElementById(
-    "comments-num"
-  ).innerText = `${commentsArr.length} comments`;
+    "questions-num"
+  ).innerText = `${questionsArr.length} 則 Q&A`;
 
-  for (let i = 0; i < commentsArr.length; i++) {
-    let li = document.createElement("li");
-
-    let commentMainLevel = document.createElement("div");
-    commentMainLevel.setAttribute("class", "comment-main-level");
-
-    // Avatar
-    let commentAvatar = document.createElement("div");
-    commentAvatar.setAttribute("class", "comment-avatar");
-
-    let img = document.createElement("img");
-    img.src = `https://d1wan10jjr4v2x.cloudfront.net/profile/${commentsArr[i]["picture"]}`;
-    img.alt = "";
-    commentAvatar.appendChild(img);
-
-    // Content
-    let commentBox = document.createElement("div");
-    commentBox.setAttribute("class", "comment-box");
-
-    // Comment Head
-    let commentHead = document.createElement("div");
-    commentHead.setAttribute("class", "comment-head");
-
-    let commentName = document.createElement("h6");
-    commentName.setAttribute("class", "comment-name");
-    commentHead.appendChild(commentName);
-
-    let commentATag = document.createElement("a");
-    commentATag.href = `/profile.html?id=${commentsArr[i]["id"]}`; // personal profile page
-    commentATag.innerText = commentsArr[i]["name"];
-    commentName.appendChild(commentATag);
-    commentHead.appendChild(commentName);
-
-    let date = new Date(commentsArr[i]["create_time"]);
-    let createDate = `${date.getFullYear()} / ${
+  for (let i = 0; i < questionsArr.length; i++) {
+    const date = new Date(questionsArr[i]["create_time"]);
+    const createDate = `${date.getFullYear()} / ${
       date.getMonth() + 1
     } / ${date.getDate()}`;
-    let commentSpan = document.createElement("span");
-    commentSpan.innerText = createDate;
-    commentHead.appendChild(commentSpan);
 
-    // let commentReply = document.createElement("i");
-    // commentReply.setAttribute("class", "fa fa-reply");
-    // commentHead.appendChild(commentReply);
+    const containerDiv = document.createElement("div");
+    containerDiv.setAttribute("class", "container");
+    containerDiv.innerHTML = `<div class="avatar">
+    <img href="/profile.html?id=${questionsArr[i]["id"]}" src="https://d1wan10jjr4v2x.cloudfront.net/profile/${questionsArr[i]["picture"]}" style="width: 100%" />
+  </div>
+  <div class="text">
+    <h3>${questionsArr[i]["name"]} <span class="time-right">${createDate}</span></h3>
+    <hr />
+    <p>
+      ${questionsArr[i]["content"]}
+    </p>
+  </div>`;
 
-    // let commentHeart = document.createElement("i");
-    // commentHeart.setAttribute("class", "fa fa-heart");
-    // commentHead.appendChild(commentHeart);
+    document.getElementsByClassName("containers")[0].appendChild(containerDiv);
+  }
 
-    commentBox.appendChild(commentHead);
+  // Rating
+  if (status == "course_before_pay") {
+    const button = document.getElementById("myBtn");
+    button.remove();
+  }
 
-    // Comment Content
-    let commentContent = document.createElement("div");
-    commentContent.setAttribute("class", "comment-content");
-    commentContent.innerText = commentsArr[i]["content"];
-    commentBox.appendChild(commentContent);
+  document.getElementById(
+    "rating-num"
+  ).innerText = `${ratingArr.length}則 評論`;
 
-    commentMainLevel.appendChild(commentAvatar);
-    commentMainLevel.appendChild(commentBox);
-    li.appendChild(commentMainLevel);
+  let total = 0;
+  for (let i = 0; i < ratingArr.length; i++) {
+    const date = new Date(ratingArr[i]["create_time"]);
+    const createDate = `${date.getFullYear()} / ${
+      date.getMonth() + 1
+    } / ${date.getDate()}`;
 
-    document.getElementById("comments-list").appendChild(li);
+    let star = "";
+    total += ratingArr[i].star;
+    for (let j = 0; j < ratingArr[i].star; j++) {
+      star += `<span class="fa fa-star checked"></span>`;
+    }
+    for (let j = 0; j < 5 - ratingArr[i].star; j++) {
+      star += `<span class="fa fa-star"></span>`;
+    }
+
+    const containerDiv = document.createElement("div");
+    containerDiv.setAttribute("class", "container");
+    containerDiv.innerHTML = `<div class="avatar">
+    <img href="/profile.html?id=${ratingArr[i]["id"]}" src="https://d1wan10jjr4v2x.cloudfront.net/profile/${ratingArr[i]["picture"]}" style="width: 100%" />
+  </div>
+  <div class="text">
+    <h3>${ratingArr[i]["name"]} <span class="time-right">${createDate}</span></h3>
+    <div class="user-rating">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      ${star}
+    </div>
+    <hr />
+    <p>
+      ${ratingArr[i]["content"]}
+    </p>
+  </div>`;
+
+    document.getElementsByClassName("containers")[1].appendChild(containerDiv);
+  }
+
+  const avgP = document.createElement("p");
+  console.log(avgP);
+  avgP.innerText = `平均：${total / ratingArr.length} ；總共 ${
+    ratingArr.length
+  } 人評分`;
+  document.getElementsByClassName("rating-avg")[0].appendChild(avgP);
+
+  // Discussion
+  if (status == "course_before_pay") {
+    document.getElementsByClassName("containers")[2].innerText =
+      "請先購買才可以看到上課討論哦～ 感謝 :)";
+  } else {
+    const inputDiv = document.createElement("div");
+    inputDiv.setAttribute("class", "container");
+    inputDiv.innerHTML = `
+    <div class="avatar">
+      <img src="./profile/a.jpg" style="width: 100%" />
+    </div>
+    <div class="input-area">
+      <div class="text">
+        <h3>Amy</h3>
+        <hr />
+        <textarea class="comment-area"> </textarea>
+      </div>
+      <button
+        type="button"
+        class="comment-btn"
+        onclick="sendDiscussion()"
+      >
+        Submit
+      </button>
+    </div>
+  `;
+
+    document.getElementsByClassName("containers")[2].prepend(inputDiv);
+
+    document.getElementById(
+      "discussion-num"
+    ).innerText = `${discussionArr.length} 則 討論`;
+
+    for (let i = 0; i < discussionArr.length; i++) {
+      const date = new Date(discussionArr[i]["create_time"]);
+      const createDate = `${date.getFullYear()} / ${
+        date.getMonth() + 1
+      } / ${date.getDate()}`;
+
+      const containerDiv = document.createElement("div");
+      containerDiv.setAttribute("class", "container");
+      containerDiv.innerHTML = `<div class="avatar">
+      <img href="/profile.html?id=${discussionArr[i]["id"]}" src="https://d1wan10jjr4v2x.cloudfront.net/profile/${discussionArr[i]["picture"]}" style="width: 100%" />
+    </div>
+    <div class="text">
+      <h3>${discussionArr[i]["name"]} <span class="time-right">${createDate}</span></h3>
+      <hr />
+      <p>
+        ${discussionArr[i]["content"]}
+      </p>
+    </div>`;
+
+      document
+        .getElementsByClassName("containers")[2]
+        .appendChild(containerDiv);
+    }
   }
 };
 
-const fontendSendComment = (event) => {
+// Change Course Information
+function openPage(pageName, elmnt, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("course-tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+// Post Question
+const sendQuestion = (event) => {
   const content = document.getElementsByTagName("textarea")[0].value;
   const searchParams = new URLSearchParams(window.location.search);
   const id = searchParams.get("id");
@@ -129,6 +237,7 @@ const fontendSendComment = (event) => {
   const comment = {
     commentedId: id,
     content: content,
+    commentTypeId: 2,
   };
 
   fetch(`/api/1.0/comment`, {
@@ -145,6 +254,63 @@ const fontendSendComment = (event) => {
     .catch((error) => console.log("Error:", error));
 };
 
+// Post Rating
+const sendRating = (event) => {
+  const content = document.getElementsByTagName("textarea")[1].value;
+  const select = document.getElementById("comment-star");
+  const star = select.options[select.selectedIndex].value;
+
+  const searchParams = new URLSearchParams(window.location.search);
+  const id = searchParams.get("id");
+
+  const comment = {
+    commentedId: id,
+    content: content,
+    commentTypeId: 1,
+    star: star,
+  };
+
+  fetch(`/api/1.0/comment`, {
+    method: "POST",
+    headers: new Headers({
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      "content-type": "application/json",
+    }),
+    body: JSON.stringify(comment),
+  })
+    .then((res) => {
+      history.go(0);
+    })
+    .catch((error) => console.log("Error:", error));
+};
+
+// Post Discussion
+const sendDiscussion = (event) => {
+  const content = document.getElementsByTagName("textarea")[2].value;
+  const searchParams = new URLSearchParams(window.location.search);
+  const id = searchParams.get("id");
+
+  const comment = {
+    commentedId: id,
+    content: content,
+    commentTypeId: 3,
+  };
+
+  fetch(`/api/1.0/comment`, {
+    method: "POST",
+    headers: new Headers({
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      "content-type": "application/json",
+    }),
+    body: JSON.stringify(comment),
+  })
+    .then((res) => {
+      history.go(0);
+    })
+    .catch((error) => console.log("Error:", error));
+};
+
+// Add Favorites
 const addToFavorites = (event) => {
   const searchParams = new URLSearchParams(window.location.search);
   const body = { id: parseInt(searchParams.get("id")) };
@@ -163,6 +329,7 @@ const addToFavorites = (event) => {
     .catch((error) => console.log("Error:", error));
 };
 
+// Add to Cart
 const addToCart = (event) => {
   const searchParams = new URLSearchParams(window.location.search);
   const body = { id: parseInt(searchParams.get("id")) };
@@ -181,27 +348,28 @@ const addToCart = (event) => {
   alert("已加入購物車");
 };
 
-// slight change from the video, where I've set the eventListener
-// for the animationend to remove itself after the click
+// Get the modal
+var modal = document.getElementById("myModal");
 
-const rippleButton = document.querySelector(".course-button");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-function mousePositionToCustomProp(event, element) {
-  let posX = event.offsetX;
-  let posY = event.offsetY;
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  element.style.setProperty("--x", posX + "px");
-  element.style.setProperty("--y", posY + "px");
-}
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
 
-rippleButton.addEventListener("click", (e) => {
-  mousePositionToCustomProp(e, rippleButton);
-  rippleButton.classList.add("pulse");
-  rippleButton.addEventListener(
-    "animationend",
-    () => {
-      rippleButton.classList.remove("pulse");
-    },
-    { once: true }
-  );
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
--- server/models/comment_model.js
+++ server/models/comment_model.js
@@ -14,37 +14,52 @@ const createComment = async (comment, token) => {
 
     const commentWithId = {
       commented_id: comment.commentedId,
+      commented_type_id: comment.commentTypeId,
       user_id: decoded.userId,
       content: comment.content,
       create_time: Date.now(),
     };
 
-    const [[getUserStatus]] = await conn.query(
-      "SELECT user.id, user.email, course_student.course_id \
-      FROM user \
-      LEFT JOIN course_student \
-      ON user.id=course_student.user_id \
-      WHERE email= ? && course_id=?",
-      [decoded.email, comment.commentedId]
-    );
+    // const [[getUserStatus]] = await conn.query(
+    //   "SELECT user.id, user.email, course_student.course_id \
+    //   FROM user \
+    //   LEFT JOIN course_student \
+    //   ON user.id=course_student.user_id \
+    //   WHERE email= ? && course_id=?",
+    //   [decoded.email, comment.commentedId]
+    // );
 
-    let commentedTypeSql = "";
-    if (!getUserStatus) {
-      commentedTypeSql = "course_before_pay";
-    } else {
-      commentedTypeSql = "course_after_pay";
-    }
+    // let commentedTypeSql = "";
+    // if (!getUserStatus) {
+    //   commentedTypeSql = "course_before_pay";
+    // } else {
+    //   commentedTypeSql = "course_after_pay";
+    // }
 
-    const [[commentedTypeId]] = await conn.query(
-      "SELECT id FROM commented_type WHERE type = ?",
-      [commentedTypeSql]
-    );
-    commentWithId.commented_type_id = commentedTypeId.id;
+    // const [[commentedTypeId]] = await conn.query(
+    //   "SELECT id FROM commented_type WHERE type = ?",
+    //   [commentedTypeSql]
+    // );
+    // commentWithId.commented_type_id = commentedTypeId.id;
 
     const [result] = await conn.query(
       "INSERT INTO comment SET ?",
       commentWithId
     );
+
+    if (comment.commentTypeId == 1) {
+      const ratingWithId = {
+        course_id: comment.commentedId,
+        user_id: decoded.userId,
+        star: comment.star,
+        create_time: Date.now(),
+      };
+
+      const [ratingResult] = await conn.query(
+        "INSERT INTO course_rating SET ?",
+        ratingWithId
+      );
+    }
 
     await conn.query("COMMIT");
     return result.insertId;
