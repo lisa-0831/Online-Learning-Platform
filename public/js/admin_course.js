534e8b89ece0f95789ce950e6c5ee5b196bac98e
PEI-WEN WANG <peiwen0831@gmail.com>
2022-07-10 20:59:38 +0800

    style: redesign upload video page

--- public/js/admin_course.js
+++ public/js/admin_course.js
@@ -1,10 +1,11 @@
 const addHashtagCol = () => {
   // Create div
-  var div = document.createElement("div");
+  const div = document.createElement("div");
+  div.setAttribute("class", "col-75");
   div.id = "hashtagCol";
 
   // Hashtag
-  var input = document.createElement("input");
+  const input = document.createElement("input");
   input.type = "text";
   input.name = "hashtag";
   input.placeholder = "#DigitalMarketing";
@@ -13,4 +14,27 @@ const addHashtagCol = () => {
 
   // div.innerHTML = '<button type="button" onclick="removeField">Remove</button>'
   document.getElementById("hashtagCols").appendChild(div);
+};
+
+const addVideoCol = () => {
+  // Create div
+  const div = document.createElement("div");
+  div.setAttribute("class", "col-75");
+  div.id = "videoCol";
+
+  // Chapter Title
+  const inputText = document.createElement("input");
+  inputText.type = "text";
+  inputText.name = "chapterTitle";
+  inputText.placeholder = "章節標題";
+  div.appendChild(inputText);
+
+  // Video List
+  const inputVideo = document.createElement("input");
+  inputVideo.type = "file";
+  inputVideo.name = "videoList";
+  div.appendChild(inputVideo);
+
+  // div.innerHTML = '<button type="button" onclick="removeField">Remove</button>'
+  document.getElementById("videoCols").appendChild(div);
 };
