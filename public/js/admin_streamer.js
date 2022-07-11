0c436c76d51920b12787b628ef1df4462b0dedeb
PEI-WEN WANG <peiwen0831@gmail.com>
2022-07-11 21:24:46 +0800

    feat: add admin livestream page

--- public/js/admin_streamer.js
+++ public/js/admin_streamer.js
@@ -1,4 +1,118 @@
-const courseId = 10;
+let courseId;
+
+window.onload = async function () {
+  let params = new URL(document.location).searchParams;
+  courseId = params.get("id");
+  console.log(courseId);
+
+  const livestreamRes = await fetch(
+    `/api/1.0/livestreams/details?id=${courseId}`,
+    {
+      method: "GET",
+      headers: new Headers({
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        "content-type": "application/json",
+      }),
+    }
+  );
+  const livestreamObj = await livestreamRes.json();
+  const detailsObj = livestreamObj.details;
+  console.log(livestreamObj);
+  console.log(detailsObj);
+
+  // Timestamp To Date
+  const timestamp2Date = (timestamp) => {
+    const date = new Date(timestamp);
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  };
+
+  // Course Information - Left
+
+  // Course Introduction
+  const introductionDiv = document.createElement("div");
+  introductionDiv.setAttribute("class", "course-info-detail");
+  introductionDiv.innerText = detailsObj.introduction;
+  document
+    .getElementsByClassName("course-introduction")[0]
+    .appendChild(introductionDiv);
+
+  // Course description
+  const descriptionDiv = document.createElement("div");
+  descriptionDiv.setAttribute("class", "course-info-detail");
+  descriptionDiv.innerText = detailsObj.description;
+  document
+    .getElementsByClassName("course-description")[0]
+    .appendChild(descriptionDiv);
+
+  // Course preparation
+  const preparationDiv = document.createElement("div");
+  preparationDiv.setAttribute("class", "course-info-detail");
+  preparationDiv.innerText = detailsObj.preparation;
+  document
+    .getElementsByClassName("course-preparation")[0]
+    .appendChild(preparationDiv);
+
+  // Course Information - Right
+
+  // Course Title
+  document.getElementsByClassName("course-title")[0].innerText =
+    detailsObj.title;
+
+  // Course Teacher
+  const teacherDiv = document.createElement("div");
+  teacherDiv.setAttribute("class", "course-action-detail");
+  teacherDiv.innerText = `直播老師：${detailsObj.teacher_name}`;
+  document
+    .getElementsByClassName("course-action-content")[0]
+    .appendChild(teacherDiv);
+
+  // Course Time
+  const startTimeDiv = document.createElement("div");
+  startTimeDiv.setAttribute("class", "course-action-detail");
+  startTimeDiv.innerText = `直播時間：${timestamp2Date(detailsObj.start_time)}`;
+  document
+    .getElementsByClassName("course-action-content")[0]
+    .appendChild(startTimeDiv);
+
+  // Student Num
+  const studentNumDiv = document.createElement("div");
+  studentNumDiv.setAttribute("class", "course-action-detail");
+  studentNumDiv.innerText = `預約人數：目前 ${detailsObj.students_num} 人預約`;
+  document
+    .getElementsByClassName("course-action-content")[0]
+    .appendChild(studentNumDiv);
+
+  // Set the date we're counting down to
+  // let countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+  let countDownDate = detailsObj.start_time;
+
+  // Update the count down every 1 second
+  let x = setInterval(function () {
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML =
+      days + "天 " + hours + "時 " + minutes + "分 " + seconds + "秒 ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "直播已經開始囉";
+    }
+  }, 1000);
+};
 
 const peerConnections = {};
 const config = {
@@ -11,9 +125,16 @@ const config = {
 
 const videoGrid = document.getElementById("video-grid");
 const video = document.createElement("video");
+// video.setAttribute("controls");
+// video.setAttribute("width", "1280");
+// video.setAttribute("height", "720");
+// video.setAttribute("id", "video-controls");
 
 const constraints = {
-  video: true,
+  video: {
+    width: { min: 1024, ideal: 1280, max: 1920 },
+    height: { min: 576, ideal: 720, max: 1080 },
+  },
   audio: true,
 };
 
@@ -34,8 +155,6 @@ navigator.mediaDevices
     videostream = stream;
     video.srcObject = stream;
     addVideoStream(video, stream);
-
-    socket.emit("broadcaster", courseId);
   })
   .catch((error) => console.error(error));
 
@@ -45,61 +164,65 @@ let p = document.createElement("p");
 subtitleContainer.appendChild(p);
 recognition.start();
 
-socket.on("viewer", (id) => {
-  const peerConnection = new RTCPeerConnection(config);
-  peerConnections[id] = peerConnection;
+const startLivestream = (event) => {
+  socket.emit("broadcaster", courseId);
 
-  let stream = videostream;
-  stream.getTracks().forEach((track) => {
-    peerConnection.addTrack(track, stream);
+  socket.on("viewer", (id) => {
+    const peerConnection = new RTCPeerConnection(config);
+    peerConnections[id] = peerConnection;
+
+    let stream = videostream;
+    stream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, stream);
+    });
+
+    peerConnection
+      .createOffer()
+      .then((sdp) => peerConnection.setLocalDescription(sdp))
+      .then(() => {
+        socket.emit("offer", id, peerConnection.localDescription);
+      })
+      .catch((e) => console.log(e));
+
+    // event is called when receive an ICE candidate
+    peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        socket.emit("candidate", id, event.candidate);
+      }
+    };
+
+    // Subtitle
+    let content;
+    let previousContent;
+    recognition.addEventListener("result", (e) => {
+      p.textContent = e.results[0][0].transcript;
+      content = p.textContent;
+      if (e.results[0].isFinal) {
+        p = document.createElement("p");
+        subtitleContainer.appendChild(p);
+      }
+    });
+
+    // We need to restart the api after the user finish a sentence
+    recognition.addEventListener("end", (e) => {
+      recognizing ? recognition.start() : recognition.stop();
+      if (previousContent !== content) {
+        const subtitleContainer = document.querySelector(".subtitle");
+        subtitleContainer.textContent = content;
+        socket.emit("message", id, content);
+        previousContent = content;
+      }
+    });
   });
 
-  peerConnection
-    .createOffer()
-    .then((sdp) => peerConnection.setLocalDescription(sdp))
-    .then(() => {
-      socket.emit("offer", id, peerConnection.localDescription);
-    })
-    .catch((e) => console.log(e));
-
-  // event is called when receive an ICE candidate
-  peerConnection.onicecandidate = (event) => {
-    if (event.candidate) {
-      socket.emit("candidate", id, event.candidate);
-    }
-  };
-
-  // Subtitle
-  let content;
-  let previousContent;
-  recognition.addEventListener("result", (e) => {
-    p.textContent = e.results[0][0].transcript;
-    content = p.textContent;
-    if (e.results[0].isFinal) {
-      p = document.createElement("p");
-      subtitleContainer.appendChild(p);
-    }
+  socket.on("answer", (id, description) => {
+    peerConnections[id].setRemoteDescription(description);
   });
 
-  // We need to restart the api after the user finish a sentence
-  recognition.addEventListener("end", (e) => {
-    recognizing ? recognition.start() : recognition.stop();
-    if (previousContent !== content) {
-      const subtitleContainer = document.querySelector(".subtitle");
-      subtitleContainer.textContent = content;
-      socket.emit("message", id, content);
-      previousContent = content;
-    }
+  socket.on("candidate", (id, candidate) => {
+    peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
   });
-});
-
-socket.on("answer", (id, description) => {
-  peerConnections[id].setRemoteDescription(description);
-});
-
-socket.on("candidate", (id, candidate) => {
-  peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
-});
+};
 
 // socket.on("disconnectPeer", (id) => {
 //   peerConnections[id].close();
--- public/js/livestream.js
+++ public/js/livestream.js
@@ -1,5 +1,140 @@
-const params = new URL(document.location).searchParams;
-const courseId = params.get("id");
+let courseId;
+
+window.onload = async function () {
+  let params = new URL(document.location).searchParams;
+  courseId = params.get("id");
+
+  const livestreamRes = await fetch(
+    `/api/1.0/livestreams/details?id=${courseId}`,
+    {
+      method: "GET",
+      headers: new Headers({
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        "content-type": "application/json",
+      }),
+    }
+  );
+  const livestreamObj = await livestreamRes.json();
+  const detailsObj = livestreamObj.details;
+
+  // Timestamp To Date
+  const timestamp2Date = (timestamp) => {
+    const date = new Date(timestamp);
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  };
+
+  // Course Video
+  let courseVideo = document.createElement("source");
+  courseVideo.src = `https://d1wan10jjr4v2x.cloudfront.net/assets/${detailsObj["teaser"]}`;
+  courseVideo.type = "video/mp4";
+  document.getElementById("video-controls").appendChild(courseVideo);
+
+  // Course Information - Left
+
+  // Course Introduction
+  const introductionDiv = document.createElement("div");
+  introductionDiv.setAttribute("class", "course-info-detail");
+  introductionDiv.innerText = detailsObj.introduction;
+  document
+    .getElementsByClassName("course-introduction")[0]
+    .appendChild(introductionDiv);
+
+  // Course description
+  const descriptionDiv = document.createElement("div");
+  descriptionDiv.setAttribute("class", "course-info-detail");
+  descriptionDiv.innerText = detailsObj.description;
+  document
+    .getElementsByClassName("course-description")[0]
+    .appendChild(descriptionDiv);
+
+  // Course preparation
+  const preparationDiv = document.createElement("div");
+  preparationDiv.setAttribute("class", "course-info-detail");
+  preparationDiv.innerText = detailsObj.preparation;
+  document
+    .getElementsByClassName("course-preparation")[0]
+    .appendChild(preparationDiv);
+
+  // Course Information - Right
+
+  // Course Title
+  document.getElementsByClassName("course-title")[0].innerText =
+    detailsObj.title;
+
+  // Course Teacher
+  const teacherDiv = document.createElement("div");
+  teacherDiv.setAttribute("class", "course-action-detail");
+  teacherDiv.innerText = `直播老師：${detailsObj.teacher_name}`;
+  document
+    .getElementsByClassName("course-action-content")[0]
+    .appendChild(teacherDiv);
+
+  // Course Time
+  const startTimeDiv = document.createElement("div");
+  startTimeDiv.setAttribute("class", "course-action-detail");
+  startTimeDiv.innerText = `直播時間：${timestamp2Date(detailsObj.start_time)}`;
+  document
+    .getElementsByClassName("course-action-content")[0]
+    .appendChild(startTimeDiv);
+
+  // Student Num
+  const studentNumDiv = document.createElement("div");
+  studentNumDiv.setAttribute("class", "course-action-detail");
+  studentNumDiv.innerText = `預約人數：目前 ${detailsObj.students_num} 人預約`;
+  document
+    .getElementsByClassName("course-action-content")[0]
+    .appendChild(studentNumDiv);
+
+  // Set the date we're counting down to
+  // let countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+  let countDownDate = detailsObj.start_time;
+
+  // Update the count down every 1 second
+  let x = setInterval(function () {
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML =
+      days + "天 " + hours + "時 " + minutes + "分 " + seconds + "秒 ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "直播已經開始囉";
+    }
+  }, 1000);
+};
+
+// Add Favorites
+const addBooking = (event) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const body = { id: parseInt(searchParams.get("id")) };
+
+  fetch("/api/1.0/livestreams/book", {
+    method: "POST",
+    headers: new Headers({
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      "content-type": "application/json",
+    }),
+    body: JSON.stringify(body),
+  })
+    .then((res) => {
+      alert(res.statusText);
+    })
+    .catch((error) => console.log("Error:", error));
+};
 
 let peerConnection;
 
